b16b65db8eb1540512aff4baf3f4d23a6be8c4b2
PapaFruta <8912yl04@gmail.com>
2023-12-06 14:24:50 -0500

    fixed kudos bug

--- server/concepts/kudos.ts
+++ server/concepts/kudos.ts
@@ -1,51 +1,50 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
 
-export interface KudoDoc extends BaseDoc{
-    giver: ObjectId;
-    receiver: ObjectId;
-    task: ObjectId;
-    message: String;
+export interface KudoDoc extends BaseDoc {
+  giver: ObjectId;
+  receiver: ObjectId;
+  task: ObjectId;
+  message: String;
 }
 
-export default class KudoConcept{
+export default class KudoConcept {
+  public readonly kudos = new DocCollection<KudoDoc>("kudos");
 
-    public readonly kudos = new DocCollection<KudoDoc>("kudos");
+  async giveKudos(giver: ObjectId, receiver: ObjectId, task: ObjectId, message: string) {
+    const found = await this.kudos.readMany({ giver: giver, task: task });
 
-    async giveKudos(giver: ObjectId, receiver: ObjectId, task: ObjectId, message: string){
-        const found = await this.kudos.readMany({giver: giver, task: task});
-
-        if(found.length > 0){
-            return {'msg': "You have already given a kudos"};
-        }
-        
-        const created = await this.kudos.createOne({giver:giver, receiver:receiver, task:task, message:message})
-
-        return {msg: "Successfully Sent Kudos", kudo: created}
+    if (found.length > 0) {
+      return { msg: "You have already given a kudos" };
     }
 
-    async getKudoForTask(task: ObjectId){
-        const found = await this.kudos.readOne({task:task})
-        return found
-    }
+    const created = await this.kudos.createOne({ giver: giver, receiver: receiver, task: task, message: message });
 
-    async getReceivedKudosOfUser(user: ObjectId): Promise<KudoDoc[]> {
-        const receivedKudos = await this.kudos.readMany({ receiver: user });
-        return receivedKudos;
-    }
+    return { msg: "Successfully Sent Kudos", kudo: created };
+  }
 
-    async getGivenKudosOfUser(user: ObjectId): Promise<KudoDoc[]> {
-        const givenKudos = await this.kudos.readMany({ giver: user });
-        return givenKudos;
-    }
+  async getKudoForTask(task: ObjectId) {
+    const found = await this.kudos.readOne({ task: task });
+    return found;
+  }
 
-    async getReceivedKudosCount(user: ObjectId): Promise<number> {
-        const receivedKudos = await this.kudos.readMany({ receiver: user });
-        return receivedKudos.length;
-    }
+  async getReceivedKudosOfUser(user: ObjectId): Promise<KudoDoc[]> {
+    const receivedKudos = await this.kudos.readMany({ receiver: user });
+    return receivedKudos;
+  }
 
-    async getGivenKudosCount(user: ObjectId): Promise<number> {
-        const givenKudos = await this.kudos.readMany({ giver: user });
-        return givenKudos.length;
-    }
+  async getGivenKudosOfUser(user: ObjectId): Promise<KudoDoc[]> {
+    const givenKudos = await this.kudos.readMany({ giver: user });
+    return givenKudos;
+  }
+
+  async getReceivedKudosCount(user: ObjectId): Promise<number> {
+    const receivedKudos = await this.kudos.readMany({ receiver: user });
+    return receivedKudos.length;
+  }
+
+  async getGivenKudosCount(user: ObjectId): Promise<number> {
+    const givenKudos = await this.kudos.readMany({ giver: user });
+    return givenKudos.length;
+  }
 }
